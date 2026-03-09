d9d476398d5e04bfbc11ba23770b2184e74751d9
Yola <yolaboman@gmail.com>
2026-03-09 11:30:49 +0700

    update about page

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -2,7 +2,11 @@
     <div class ="container">
     <h1>About</h1>
     <p>
-    update about
+    Politeknik Ganesha Medan  adalah institusi pendidikan vokasi swasta 
+    terkemuka di Medan yang berfokus pada bidang informatika dan bisnis, 
+    berlokasi di Jl. Veteran No. 194. Berdiri di bawah Yayasan Ganesa Nusantara, 
+    Polgan menawarkan program D3 (Akuntansi, Teknik Informatika, Manajemen Informatika) 
+    dengan kurikulum berbasis industri, masa studi singkat (2,5 tahun), dan menjamin penempatan kerja.
     </p>
 </div>
 </x-app-layout>
