407dcf3eaa328308e005ad35b99dcd34bc4825c9
Yola <yolaboman@gmail.com>
2026-03-09 11:08:01 +0700

    update home page

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,6 +1,11 @@
 <x-app-layout>
     <h1>Home</h1>
     <p>
-    update home
+    Berikut adalah detail penting mengenai Polgan Medan:
+    Program Studi (D3): Menyelenggarakan tiga program studi utama yaitu Akuntansi, Teknik Informatika, dan Manajemen Informatika, dengan akreditasi B.
+    Keunggulan: Kuliah lebih singkat (2,5 tahun), membuka kelas karyawan, serta fokus pada keterampilan (skill), pengetahuan (knowledge), dan sikap (attitude).
+    Fasilitas: Menyediakan ruang kelas nyaman, laboratorium komputer yang memadai, perpustakaan, musholla, dan area parkir.
+    Kegiatan Akademik: Aktif mengadakan seminar, kuliah umum, bazar UMKM, serta memiliki jurnal ilmiah sendiri (Jurnal Pengabdian POLGAN/JPP).
+    Lokasi & Kontak: Berlokasi di Jalan Veteran Nomor 194 Manunggal, Medan. Pendaftaran biasanya dilakukan secara daring melalui situs resmi mereka, polgan.ac.id
     </p>
 </x-app-layout>
